16644c59fdecffc3cdf463fe5bd5c1c8d2eb7a1a
smheidrich <smheidrich@weltenfunktion.de>
2022-10-10 21:57:52 +0200

    Put test data into resource files

---
+++ tests/datafixtures/setup_without_rust_ext_optional_arg.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+from setuptools_rust import Binding, RustExtension
+
+setup(
+    name="mypkg",
+    version="0.1.0",
+    rust_extensions=[
+        RustExtension(
+            "mypkg.mypkg_rs",
+            binding=Binding.PyO3,
+        )
+    ],
+    packages=["mypkg"],
+    zip_safe=False,
+)
